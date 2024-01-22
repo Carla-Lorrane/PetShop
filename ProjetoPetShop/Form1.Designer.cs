a4578cff799156e85e4ca210201cc3db28ce6a5a
Carla Lorrane <carlalorraneweb@gmail.com>
2024-01-22 16:16:49 -0300

    pro PetShop

---
+++ ProjetoPetShop/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ProjetoPetShop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLateal = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.panelcentral = new System.Windows.Forms.Panel();
+            this.panelLateal.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLateal
+            // 
+            this.panelLateal.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panelLateal.Controls.Add(this.button9);
+            this.panelLateal.Controls.Add(this.button5);
+            this.panelLateal.Controls.Add(this.button4);
+            this.panelLateal.Controls.Add(this.button3);
+            this.panelLateal.Controls.Add(this.button2);
+            this.panelLateal.Controls.Add(this.button1);
+            this.panelLateal.Location = new System.Drawing.Point(0, 75);
+            this.panelLateal.Name = "panelLateal";
+            this.panelLateal.Size = new System.Drawing.Size(267, 756);
+            this.panelLateal.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.ForeColor = System.Drawing.Color.Gray;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1540, 76);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(36, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Inicío";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(36, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Raça";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(36, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Animal";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(36, 294);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(178, 39);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Serviços";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(36, 374);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(178, 39);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Agendamentos";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1263, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(119, 40);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "Min";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1405, 12);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(123, 40);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "Fechar";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(36, 450);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(178, 39);
+            this.button9.TabIndex = 5;
+            this.button9.Text = "Clientes\r\n";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // panelcentral
+            // 
+            this.panelcentral.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panelcentral.BackgroundImage = global::ProjetoPetShop.Properties.Resources.pexels_anna_bondarenko_11380177;
+            this.panelcentral.Location = new System.Drawing.Point(268, 75);
+            this.panelcentral.Name = "panelcentral";
+            this.panelcentral.Size = new System.Drawing.Size(1279, 766);
+            this.panelcentral.TabIndex = 2;
+            this.panelcentral.Paint += new System.Windows.Forms.PaintEventHandler(this.panelcentral_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1540, 831);
+            this.Controls.Add(this.panelcentral);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelLateal);
+            this.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelLateal.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLateal;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelcentral;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ ProjetoPetShop/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoPetShop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            if (MessageBox.Show("Deseja mesmo sair ?", "PetShop", MessageBoxButtons.YesNo, MessageBoxIcon.Question)== DialogResult.Yes)
+            Application.Exit();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelcentral_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
